802312672d0b7913e09559ab27d04720dd9d17f7
jaimin25 <jaiminchovatia25123@gmail.com>
2024-02-15 22:47:33 +0530

    feat: uploadthing setup for file uplaods

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import cors from "cors";
 import express from "express";
+import "dotenv/config";
 
-import {
-  checkForName,
-  signIn,
-  signUp,
-} from "./src/controllers/authController.js";
+import { checkForName, signIn, signUp } from "./src/controllers/authController.js";
 import { connectDb } from "./src/lib/db.js";
+import { createRouteHandler } from "uploadthing/express";
+import { uploadRouter } from "./src/uploadthing.js";
 
 connectDb();
 
@@ -14,6 +13,13 @@ const app = express();
 const PORT = 5172;
 
 app.use(cors());
+app.use(
+  "/api/uploadthing",
+  createRouteHandler({
+    router: uploadRouter,
+    config: {},
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
