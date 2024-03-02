ff42eba60547b271a2051f2784397b74cc2a5543
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-02 11:59:28 +0530

    feat: other user details schema

---
+++ src/models/otherUserDetailsModal.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const OtherUserDetailsSchema = new mongoose.Schema(
+    {
+        uid: String,
+        gender: String,
+        dob: String,
+        govtIDType: String,
+        govtIDNumber: String,
+        accountHolderName: String,
+        accountNumber: String,
+        ifscCode: String,
+        bankName: String,
+        accountType: String,
+        status: String,
+    },
+    { timestamps: true }
+);
+
+export const OtherUserDetails = mongoose.model(
+    'OtherUserDetails',
+    OtherUserDetailsSchema
+);
